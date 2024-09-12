dd172bf313ffa24650456e46291141bba53ec6f3
aviel <aviel469@gmail.com>
2024-09-12 09:56:30 +0300

    Mission6

--- exam.js
+++ exam.js
@@ -4,6 +4,13 @@ const Mission1 = (arr) => {
     return newArr
 }
 
+const Mission6  = (num) => {
+    if (num == 0)
+        return 0;
+    if (num == 1)
+        return 1;
+    return Mission6(num - 1) + Mission6(num - 2);
+}
 
 
 
