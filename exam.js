c1f60dc28315363fd694149a2b64b514dd1c4604
aviel <aviel469@gmail.com>
2024-09-12 10:07:17 +0300

    Mission5

--- exam.js
+++ exam.js
@@ -3,6 +3,15 @@ const Mission1 = (arr) => {
     const newArr = arr.filter(x => x % 2 == 0 )
     return newArr
 }
+const Mission5 = (arr1 , arr2) => {
+    let newObj = {};
+    let length = Math.min(arr1.length, arr2.length);
+    for (let i = 0; i < length; i++) {
+        newObj[arr1[i]] = arr2[i];
+    }
+    return newObj;
+}
+
 
 const Mission6  = (num) => {
     if (num == 0)
